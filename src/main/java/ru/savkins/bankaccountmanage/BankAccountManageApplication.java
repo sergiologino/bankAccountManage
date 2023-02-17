1c988fd007cec1cf23c668ed63e07275d3058bcb
sergiologino <savkin.sergey@yandex.ru>
2023-02-17 16:02:32 +0300

    Initial commit

---
+++ src/main/java/ru/savkins/bankaccountmanage/BankAccountManageApplication.java
@@ -0,0 +1,13 @@
+package ru.savkins.bankaccountmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountManageApplication.class, args);
+    }
+
+}
